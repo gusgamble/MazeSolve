94e2baea39623cb4353d54cd622a6894421b9594
Gus Gamble <lgamble20@gwu.edu>
2017-10-25 17:17:18 -0400

    just adding the class

--- Lab4/src/LineFollow.java
+++ Lab4/src/LineFollow.java
@@ -23,14 +23,15 @@ public class LineFollow {
 	static Wheel wheel2 = WheeledChassis.modelWheel(rightMotor , 3.0).offset(5.5);
 	static Chassis chassis = new WheeledChassis(new  Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
 	static MovePilot pilot = new MovePilot(chassis);
-	static double blackColorID = 7.0; //Hardcoded for now. Will change once calibration is debugged.
-	static double whiteColorID = 2.0; //Hardcoded for now. Will change once calibration is debugged.
+	static double blackColorID = 0.04; //Hardcoded for now. Will change once calibration is debugged.
+	static double whiteColorID = 0.18; //Hardcoded for now. Will change once calibration is debugged.
+	static double turnAngle = 2;
 	
 	public static void main(String[] args) {
 		Button.waitForAnyPress();
 		
 		//mode that measures reflected light
-		SensorMode getColor = colorSensor.getColorIDMode();
+		SensorMode getColor = colorSensor.getRedMode();
 		float [] samplevalue =  new float [getColor.sampleSize()];
 		
 		/**CALIBRATION
@@ -57,6 +58,8 @@ public class LineFollow {
 			float floor = getColor.sampleSize();
 		}*/
 		
+		pilot.setLinearSpeed(6);
+		
 		pilot.forward();
 		
 		while(Button.getButtons() != Button.ID_ESCAPE){                   
@@ -65,22 +68,24 @@ public class LineFollow {
 		    
 	    		if (samplevalue[0] == whiteColorID){ //normal color of floor
 	    			
-	   			pilot.rotate(-15); 
-	   			getColor.fetchSample(samplevalue, 0);
+	   			pilot.rotate(-turnAngle); 
+	   			//getColor.fetchSample(samplevalue, 0);
+	   			pilot.forward();
 	    		}
 	    		
 	   		else if (samplevalue[0] == blackColorID){ //detects the black line
 	   			
-	   			pilot.rotate(15);
-	   			getColor.fetchSample(samplevalue, 0);
+	   			pilot.rotate(turnAngle);
+	   			//getColor.fetchSample(samplevalue, 0);
+	   			pilot.forward();
 	    		}
 	    		
-	   		else if (samplevalue[0]<whiteColorID && samplevalue[0]>blackColorID) {
+	   		//else if (samplevalue[0]<whiteColorID && samplevalue[0]>blackColorID) {
 	   			
-	   			pilot.forward();
-	   			getColor.fetchSample(samplevalue, 0);
+	   			//pilot.forward();
+	   			//getColor.fetchSample(samplevalue, 0);
 	   			
-	   		}
+	   		//}
 		}
 	}
 }
