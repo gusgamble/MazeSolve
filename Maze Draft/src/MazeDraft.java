76283ef624b788ede91aa216e1aba5d1d0ff6a6d
Sam Frey <samfrey99@gwu.edu>
2017-11-06 17:14:59 -0500

    removed duplicate method

--- Maze Draft/src/MazeDraft.java
+++ Maze Draft/src/MazeDraft.java
@@ -122,42 +122,6 @@ public class MazeDraft {
 		return (samplevalue[0]==1);
 	}
 
-	public static double[] RGBtoHSV(Color colors){
-		double[] hsv = new double[3];
-		// read colors
-		int r = colors.getRed();
-		int b = colors.getBlue();
-		int g = colors.getGreen();
-		
-		double min = Math.min(r, Math.min(b,g));
-		double max = Math.max(r, Math.max(b, g));
-		double delta = max - min;
-		hsv[2] = max/255; //set v to max as a percentage
-		if (max != 0){
-			hsv[1] = delta/max;
-		}
-		else{ //r = b = g =0 
-			hsv[1] = 0; //s = 0;		// s = 0, v is undefined
-			hsv[0] = -1; //h = -1;
-			return hsv;
-		}
-		
-		if (r == max){
-			hsv[0] = (g-b)/delta; //h 
-		}
-		else{
-			if (g == max)
-				hsv[0] = 2 + (b - r)/delta; //h
-			else
-				hsv[0] = 4 + (r - g)/delta; //h
-		}
-		
-		hsv[0] *=60;	//degrees
-		if (hsv[0] < 0)
-			hsv[0] +=360;
-		
-		return hsv;
-	}
 	
 	/* THIS IS WHERE THE CODE FOR THE LINE FOLLOW GOES
 	public static boolean isOffLine(SensorMode sensor) {
