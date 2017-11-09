81781d81692ee9bc9d5a6b2e3c93f8a8ffd35291
Gus Gamble <lgamble20@gwu.edu>
2017-11-09 11:38:02 -0500

    this is semi working, but i have added cemments to describe in the code

--- Maze Draft/src/FollowHSV.java
+++ Maze Draft/src/FollowHSV.java
@@ -36,43 +36,60 @@ public class FollowHSV {
 	
 public static void main(String[] args) throws InterruptedException {
 		
+		//here we set the speed for the robot before anything else happens
 		pilot.setLinearSpeed(SPEED);
 		Button.waitForAnyPress();
 		
+		//we assign values to the "places" or colors on the maze. This does not get blue or foil
 		calibrateColor(pilot);
 		
 		Color rgb;
 		
+		//this is where we begin motion
 		pilot.forward();
 		
-		//double boundary = 5;
-		
-		while(Button.getButtons() != Button.ID_ESCAPE){ 
+		while(Button.getButtons() != Button.ID_ESCAPE){ //infinite loop
 			
-			
+			//setting the color sensor up to get a new value every time this loops 
 			rgb = color_sensor.getColor();
 			double[] samplevalue = RGBtoHSV(rgb);
 		    System.out.println(samplevalue[0]);
 		    
-	    		if ((samplevalue[0]-woodHSV[0])<=TOLERANCE || (samplevalue[0]-woodHSV[0])>TOLERANCE){ //normal color of floor
+		    if ((samplevalue[0] >= (lineHSV[0]-10)) && (samplevalue[0] <= (lineHSV[0]+10))){ //if on the color that is between the line and wood
+	    		//it is important to note that the colors, UNLIKE getRedMode() are NOT numerically related to each other.
+		    	//We need to say that the robot is ON the wood or ON the line or ON the black to have the proper movements
 	    			
-	    			
+		    		//both motors, since we are on the line, are moving forward. This looks like the robot going in a straight line
+		    		
+		    		left_motor.setSpeed((int)90);
+		    		right_motor.setSpeed((int)90);
 	    			left_motor.forward();
 	    			right_motor.forward();
-	    			left_motor.setSpeed((int)90);
-	    			right_motor.setSpeed((int)90);
 	    			
-	    			System.out.println(samplevalue[0]);
+	    			
+	    			System.out.println(samplevalue[0]);//print stub
 	    		}
 	    		
-	   		else if ((samplevalue[0]-blackHSV[0])<=TOLERANCE || (samplevalue[0]-blackHSV[0])>TOLERANCE){ 
+	   		else if ((samplevalue[0]>=(blackHSV[0]-5)) && (samplevalue[0] <= (blackHSV[0]+5))){ //if the color sensor sees the black line (the tolerance is there so we can make better movements), turn right (left motor forward, right motor back)
+	   			//NOTE: the tolerance might need to be smaller or larger here. We followed a line through one intersection correctly,
+	   			//but got to a less defined one and the robot only went back and forth
 	   	
-	   			left_motor.forward();
-	   			left_motor.setSpeed((int)200);
-	   			right_motor.backward();
-	   			System.out.println(samplevalue[0]);	
-	    		}	   		
-	   		else {
+	   			left_motor.forward();//this is to make sure that the left motor is going forward, just in case
+	   			left_motor.setSpeed((int)200);//this speed is subject to change. The casting of (int) is to make sure the we are using the correct setSpeed. there is a setSpeed that uses float, but we are using int for consistency 
+	   			right_motor.backward();//this is to set the right motor to move backwards, to make right angles possible
+	   			System.out.println(samplevalue[0]);//print stub	
+	    		}
+		    
+	   		else if ((samplevalue[0]>=(woodHSV[0]-5)) && (samplevalue[0] <= (woodHSV[0]+5))){ //if the color sensor sees the wood (with tolerance), we need to turn left (left motor backwards, right motor forwards) to get back on the line
+	   			
+	   			right_motor.forward();//making sure the the right motor is still moving forward
+	   			right_motor.setSpeed((int)300);//speeding the right motor up to make the turn
+	   			left_motor.backward();//setting the left motor to move backwards so we can make right angle turns
+	   			System.out.println(samplevalue[0]);//print stub
+
+	   		}
+		    
+	   		else {//THIS ELSE STATEMENT IS A CATCH ALL. AS OF NOW, IT IS REACHABLE BUT THAT BEHAVIOR IS NOT WHAT WE NEED. THIS IS LEFT OVER FROM getRedMode() WHERE IT WAS UNREACHABLE, BUT IMPLEMENTED TO PREVENT POSSIBLE BAD BEHAVIOR
 	   			left_motor.setSpeed((int)90);
 	   			right_motor.setSpeed((int)90);
 	   			System.out.println(samplevalue[0]);
@@ -99,26 +116,29 @@ public static void backUp(MovePilot pilot){
 public static void calibrateColor(MovePilot pilot) throws InterruptedException
 {
 	color_sensor.setRGBMode();
+	color_sensor.setFloodLight(Color.WHITE);//this is for 
+	//also we can use the getAmbientLight to get the foil
+	Thread.sleep(100);
 	
 	lineHSV = RGBtoHSV(color_sensor.getColor());
 	System.out.println(lineHSV[0]);
-	Thread.sleep(500);
+	Thread.sleep(100);
 	
-	pilot.rotate(-25);
+	pilot.rotate(-30);
 	Thread.sleep(100);
 	
 	blackHSV = RGBtoHSV(color_sensor.getColor());
 	System.out.println(blackHSV[0]);
-	Thread.sleep(500);
+	Thread.sleep(100);
 	
-	pilot.rotate(50);
+	pilot.rotate(60);
 	Thread.sleep(100);
 	
 	woodHSV = RGBtoHSV(color_sensor.getColor());
 	System.out.println(woodHSV[0]);
-	Thread.sleep(500);
+	Thread.sleep(100);
 	
-	pilot.rotate(-25);
+	pilot.rotate(-30);
 }
 
 public static double[] RGBtoHSV(Color colors){
